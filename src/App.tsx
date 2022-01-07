c636232e5f03baddf29b95eb56c7bbc5f7b48f8d
Victor Fei <vf63@cornell.edu>
2022-01-06 17:39:09 -0800

    simple connection made

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,34 @@
-import React, { createContext, useContext } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {authenticateUser} from './DidHelper';
 import Button from "./components/Button";
-import { DidContextProvider, DidContext } from './DidContext';
+function App() {
+  const [did, setDid] = useState('None');
 
-
-const ConnectWallet = () => {
-  console.log('You clicked on the button!');
-  const {setDid} = useContext(DidContext);
+  const connectDidWallet = () => {
   authenticateUser().then(
     (id) => {
-      console.log('ConnectWallet: Connected with DID:', id)
-      // setDid(id);
+      console.log('ConnectWallet: Connected with DID:', id);
+      setDid(id);
     },
     (err) => {
       console.error('ConnectWallet: Failed to authenticate:', err)
     }
   );
-
 }
-
-const DidConnectionStatus = () => {
-  const {did} = useContext(DidContext);
-  const handleOnClick = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    ConnectWallet();
-    setDid(did);
-    console.log('setDid connected, button, did:', did);
-  };
 
   return (
     <>
-        <h1>Connected: {did} </h1>
-        <button onClick={handleOnClick}>Connect to wallet</button> */}
-     </>
-  );
-}
-
-function App() {
-  console.log("App, window.idx=", window.idx);
-  console.log("App, window.did=", window.did);
-  return (
-    <>
-      <h1>Colorful Custom Button Components</h1>
-      <DidContextProvider>
-        <DidConnectionStatus/>
-
+      <h4>Connected: {did} </h4>
       <Button 
         border="none"
-        color="pink"
+        color="aqua"
         height = "200px"
-        onClick={ConnectWallet}
+        onClick={connectDidWallet}
         radius = "50%"
         width = "200px"
-        children = "I'm a pink circle!"
+        children = "Click to connect your crypto wallet."
       />
-      </DidContextProvider>
     </>
   );
 }
